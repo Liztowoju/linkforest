4b7ec4eed641d6605f69e3f8844443d4e490fe82
Liz Towoju <liztowoju@gmail.com>
2022-11-01 09:49:00 +0100

    added links

--- src/App.js
+++ src/App.js
@@ -7,21 +7,36 @@ import './App.css';
 
 function App() {
   const [links, setLinks] = useState([
-    { name: 'Zuri Link', url: 'https://google.com' }
+    { name: 'Twitter Link', url: 'https://google.com' },
+    { name: 'Zuri Team', url: 'https://google.com' },
+    { name: 'Zuri Books', url: 'https://google.com' },
+    { name: 'Python Books', url: 'https://google.com' },
+    { name: 'Background Check for Coders', url: 'https://google.com' },
+    { name: 'Design Books', url: 'https://google.com' }
   ]);
-
 
   return (
     <div className="app">
       <header className="app-header">
-        <img src={avatar} className="app-logo rounded-circle" alt="logo" />
-        <p className='app-profile-name'>
-          Elizabeth Towoju
-        </p>
-        <img src={shareIcon} className="app-share-link" />
+        <div className='container col-xxl-8 position-relative'>
+          <img src={avatar} className="app-logo rounded-circle" alt="logo" />
+          <p className='app-profile-name'>
+            Elizabeth Towoju
+          </p>
+          <img src={shareIcon} className="app-share-link" />
+        </div>
       </header>
 
       <main>
+        <div className='container col-xxl-8'>
+          <div className="app-links  list-group">
+            {links.map((item, i) => (
+              <a href="#" className="app-link" aria-current="true" key={i}>
+                {item.name}
+              </a>
+            ))}
+          </div>
+        </div>
       </main>
     </div>
   );
